3ca5d9ecbfb10903a0f12edfe94a5c3557800f90
Chris Hundley <bottompair@gmail.com>
2012-08-13 15:39:03 -0700

    Added costs data for COGS calculations

---
+++ data_access/model/cost_model.js
@@ -0,0 +1,24 @@
+﻿/**
+* Model for cost collection (for calculating TCO)
+*/
+mongoose = require('mongoose');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId,
+    Model = mongoose.Model;
+
+var CostSchema = new Schema({
+    importDT: Date,
+    vendor: String,
+    type: String,
+    detail: String,
+    monthlyCost: Number,
+    percSEO: Number,
+    percTraffic: Number,
+    percEmail: Number,
+    percOverhead: Number,
+    notes: String
+});
+
+var CostModel = mongoose.model('Cost', CostSchema);
+exports.CostModel = CostModel;
