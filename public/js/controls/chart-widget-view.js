d82bf593cf8093ae38602efbaaffe2ada793d53b
Chris Hundley <bottompair@gmail.com>
2012-09-05 07:19:59 -0700

    Added missing widget class from previous checkin

---
+++ public/js/controls/chart-widget-view.js
@@ -0,0 +1,20 @@
+﻿if (!window.Opty) { window.Opty = {}; }
+
+Opty.ChartWidgetView = Backbone.View.extend({
+    className: 'widget-white',
+    initialize: function (options) {
+        var me = this;
+        me.options = options;
+        _.bindAll(this, 'render');
+        me.render();
+    },
+
+    render: function () {
+        var me = this;
+
+        $divChartWidget = $('<div>', { 'class': 'widget-body' });
+        $divChartWidget.append(me.options.chart.$el);
+        this.$el.append($divChartWidget);
+        return this.$el;
+    },
+});
